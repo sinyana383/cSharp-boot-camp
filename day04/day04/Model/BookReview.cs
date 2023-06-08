56cb0b2f53c2f624d4d3ef1f20b1851646a81bad
ddurrand <ddurrand@student.21-school.ru>
2023-06-08 14:05:31 +0300

    ex00 done

--- day04/day04/Model/BookReview.cs
+++ day04/day04/Model/BookReview.cs
@@ -6,7 +6,7 @@ public class BookReview
 {
     // the title of the book, its author, description, place in the rating, its (rating) title,
     // link to a page in the store.
-
+    
     public class BookDetails
     {
         [JsonPropertyName("title")]
@@ -18,9 +18,9 @@ public class BookReview
         [JsonPropertyName("description")]
         public string SummaryShort {get; set;}
         
-        [JsonPropertyName("rank")]
-        public string Rank {get; set;}
     }
+    [JsonPropertyName("rank")]
+    public int Rank {get; set;}
     
     [JsonPropertyName("book_details")]
     public List<BookDetails> Book {get; set; }
@@ -33,7 +33,7 @@ public class BookReview
 
     public override string ToString()
     {
-        return $"{Book[0].Title} by {Book[0].Author} [{Book[0].Rank} on NYT’s {ListName}]\n" +
+        return $"{Book[0].Title} by {Book[0].Author} [{Rank} on NYT’s {ListName}]\n" +
         $"{Book[0].SummaryShort}\n" +
         $"{Url}\n";
     }
