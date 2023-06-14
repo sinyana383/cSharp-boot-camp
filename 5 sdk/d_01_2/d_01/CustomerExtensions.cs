545bab2941088e786a780c6c29c9dc9eae07e17b
ddurrand <ddurrand@student.21-school.ru>
2023-06-14 18:08:41 +0300

    ?

---
+++ 5 sdk/d_01_2/d_01/CustomerExtensions.cs
@@ -0,0 +1,34 @@
+namespace d_01;
+
+public static class CustomerExtensions
+{
+    public static CashRegister? LeastCustomersNumber(HashSet<CashRegister> setOfRegisters)
+    {
+        if (setOfRegisters.Count < 1)
+            return null;
+        
+        CashRegister minCustomers = setOfRegisters.First();
+        foreach (CashRegister cur in setOfRegisters)
+        {
+            if (cur.CustomersNumber < minCustomers.CustomersNumber)
+                minCustomers = cur;
+        }
+
+        return minCustomers;
+    }
+    
+    public static CashRegister? LeastGoodsNumber(HashSet<CashRegister> setOfRegisters)
+    {
+        if (setOfRegisters.Count < 1)
+            return null;
+        
+        CashRegister minGoods = setOfRegisters.First();
+        foreach (CashRegister cur in setOfRegisters)
+        {
+            if (cur.GetGoodsNumberFromQueue() < minGoods.GetGoodsNumberFromQueue())
+                minGoods = cur;
+        }
+
+        return minGoods;
+    }
+}
